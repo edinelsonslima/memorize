f8bf6ea2743d9d57d7296ba63c5643d3cce35aa1
edinelsonslima <edinelsonlima86@gmail.com>
2022-06-08 00:18:40 -0300

    Second class, theory and practice about mvvm

---
+++ memorize/MemoryGame.swift
@@ -0,0 +1,35 @@
+//
+//  MemorizeGame.swift
+//  memorize
+//
+//  Created by Edinelson Lima on 07/06/22.
+//
+
+import Foundation
+ 
+struct MemoryGame<CardContent > {
+    var cards: Array<Card>
+    
+    func choose(card: Card){
+        print("card chosen : \(card)")
+    }
+    
+    init (numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card( content: content, id: pairIndex * 2 ))
+            cards.append(Card( content: content, id: pairIndex * 2 + 1 ))
+
+        }
+    }
+    
+    struct Card: Identifiable{
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
+ 
