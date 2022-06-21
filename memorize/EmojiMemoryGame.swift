7cfb5ec41cd36c4dc9d04820a0e93791b6a92dc0
edinelsonslima <edinelsonlima86@gmail.com>
2022-06-21 19:50:21 -0300

    Fifth class, ViewBuilder + Shape + ViewModifier

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
 //  MARK: - create card with one par
-    static func createMemoryGame() -> MemoryGame<String>{
+    private static func createMemoryGame() -> MemoryGame<String>{
         let emojis = ["👻","🎃","🕷"]
         return MemoryGame<String>(numberOfPairsOfCards: emojis.count){ pairIndex in
            emojis[pairIndex]
